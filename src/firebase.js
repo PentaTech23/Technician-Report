634efc2067410e7ac1a4c82209b2ef2e99188ed1
GianCarloHern <giancarlo.hernandez.u@bulsu.edu.ph>
2023-11-29 03:03:32 +0800

    Added stuff to Borrower's form. Cleaned the Firebase config and imports of borrowers form

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,42 @@
-import { getAuth, onAuthStateChanged } from '@firebase/auth'
-import { initializeApp } from "firebase/app";
-import { useState, useEffect, useContext, createContext } from 'react'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { useState, useEffect, useContext, createContext } from 'react';
+import { getFirestore, collection, doc } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
-const firebaseApp = initializeApp({
+const firebaseConfig  = {
   apiKey: "AIzaSyDHFEWRU949STT98iEDSYe9Rc-WxcL3fcc",
   authDomain: "wp4-technician-dms.firebaseapp.com",
   projectId: "wp4-technician-dms",
   storageBucket: "wp4-technician-dms.appspot.com",
   messagingSenderId: "1065436189229",
   appId: "1:1065436189229:web:88094d3d71b15a0ab29ea4"
-})
+}
+
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize Firestore db
+const db = getFirestore(firebaseApp);
+
+// Access main collection
+const mainCollectionRef = collection(db, "WP4-TESTING-AREA");
+
+// Access FORMS document under main collection
+const formsDocRef = doc(mainCollectionRef, "FORMS");
+
+// Add to subcollection 
+const BorrowersCollectionRef = collection(formsDocRef, "ITEM-BORROWERS");
+
+// Access ARCHIVES document under main collection
+const archivesRef = doc(mainCollectionRef, "ARCHIVES");
+
+const archivesCollectionRef = collection(archivesRef, "ARCHIVES-FORMS");
+
+// Initialize Storage
+const storage = getStorage(firebaseApp);
 
 export const AuthContext = createContext()
 
@@ -40,4 +64,13 @@ export const useAuthState = () => {
   return { ...auth, isAuthenticated: auth.user !=null }
 }
 
-export default firebaseApp;
+export {
+  firebaseApp,
+  db,
+  mainCollectionRef,
+  formsDocRef,
+  BorrowersCollectionRef,
+  archivesRef,
+  archivesCollectionRef,
+  storage
+};
